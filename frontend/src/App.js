4a6f8284ed941498e6317bc5e1f01c206df75109
Adharsh R <adharshrajeeev2000@gmail.com>
2023-03-13 22:34:00 +0530

    initial commit added login page using chakra ui in frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import Login from './Components/Login'
+
+function App() {
+  return (
+    <Fragment>
+
+      <Login/>
+    </Fragment>
+  )
+}
+
+export default App
